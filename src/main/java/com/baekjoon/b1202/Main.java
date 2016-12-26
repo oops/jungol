13da2d38008a7aa4935476c3e392334c53889fe4
shinoops@gmail.com <shinoops@gmail.com>
2016-12-26 14:11:06 +0900

    b1202 done

--- src/main/java/com/baekjoon/b1202/Main.java
+++ src/main/java/com/baekjoon/b1202/Main.java
@@ -10,15 +10,12 @@ public class Main {
 
     public static long[] vm ;
     public static long VMDUMMY = 10000000L;
-    public static long[] C ;
     public static int N;
     public static int K;
     public static TreeSet<Integer>  ts = new TreeSet<Integer>();
     public static Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-
     public static void main(String... arg) throws Exception{
-
 
         FileInputStream in = new FileInputStream("D:\\source\\jungol\\src\\main\\java\\com\\baekjoon\\b1202\\input.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -30,7 +27,6 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         vm = new long[N];
-        C = new long[K];
 
         int localm ;
         int localv ;
@@ -47,39 +43,33 @@ public class Main {
         Arrays.sort(vm);
 
         // 가방 무게를 treeset에 넣고 동일 무게 가방의 개수를 map에 저장
-        int localc;
+        Integer bagCapacity ;
         for (int i = 0; i < K; i++) {
-            localc = Integer.parseInt(br.readLine());
-            if( ts.contains(localc)) {
-                map.put(localc, map.get(localc) + 1 );
+            bagCapacity = Integer.parseInt(br.readLine());
+            if( ts.contains(bagCapacity)) {
+                map.put(bagCapacity, map.get(bagCapacity) + 1 );
             } else {
-                ts.add(localc);
-                map.put(localc, 1);
+                ts.add(bagCapacity);
+                map.put(bagCapacity, 1);
             }
         }
         long sum = 0L;
-        Integer bagc ;
-        int count = 0;
 
         // 가치가 높은 보석부터 그 무게를 감당할 수 있는 가장 작은 가방에 차례로 넣고
         // 해당 가방을 하나씩 뺀다. 개수를 빼거나(map) 아예 그 가방 존재 자체를 제거(treeset)
         for (int i = N-1; i >= 0; i--) {
-            localm = (int)(vm[i] % VMDUMMY);
-            localv = (int)(vm[i] / VMDUMMY);
             // 보석을 넣을 수 있는 가장 작은 가방 선택
-            bagc = ts.ceiling(localm);
+            bagCapacity = ts.ceiling((int)(vm[i] % VMDUMMY));
             // 넣을 수 있는 가방이 있다면
-            if( bagc != null ) {
-                if( map.get(bagc) > 1 ) {
-                    map.put(bagc, map.get(bagc)-1);
+            if( bagCapacity != null ) {
+                if( map.get(bagCapacity) > 1 ) {
+                    map.put(bagCapacity, map.get(bagCapacity)-1);
                 } else {
-                    ts.remove(bagc);
+                    ts.remove(bagCapacity);
                 }
-                sum += localv;
+                sum += (int)(vm[i] / VMDUMMY);
             }
         }
         System.out.println(sum);
-
     }
-
 }
