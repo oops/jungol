5abc3f795e553b6818f312b3f02f8fd5cedc0780
shinoops <oops@users.noreply.github.com>
2017-01-02 08:13:22 +0900

    Update Solution3.java

--- src/main/java/com/test/Solution3.java
+++ src/main/java/com/test/Solution3.java
@@ -39,9 +39,6 @@ public class Solution3 {
 
     public static void main(String...arg) {
         Solution3 s = new Solution3();
-//        A[0] =  1.0    A[1] = 0.1    A[2] = -1.0
-//        A[3] = -7.0    A[4] = 3.0    A[5] = -5.0
-//        A[6] = -2.5    A[7] = 0.0    A[8] =  1.0
         double[] ii = {1.0, 0.1, -1.0, -7.0, 3.0, -5.0, -2.5, 0.0, 1.0};
         double result = s.solution(ii);
         System.out.println(result);
